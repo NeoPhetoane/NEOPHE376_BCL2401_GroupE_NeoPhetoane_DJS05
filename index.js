1e11c1cb2cba5167e7b2d54a706cb71db4b5e36f
NeoPhetoane <phetoaneneo4@gmail.com>
2024-05-24 15:40:21 +0200

    Increment, decrement and reset function created

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 const initialState = { count: 0 };  //Initial state of the app declared
+
+//Counting functions
+
+// function reducer(state = initialState, actions) {
+//     switch (actions.type) {
+//       case action.INCREMENT:
+//         return { count: state.count + 2 };
+//       case action.DECREMENT:
+//         return { count: state.count - 1 };
+//       case action.RESET:
+//         return { count: 0 };
+//       default:
+//         return state;
+//     }
+//   }
+
+
+//Action functions created
+  function increment(state) {
+    return { count: state.count + 1 }
+}
+  
+function decrement(state) {
+    return { count: state.count - 1 }
+}
+
+function reset() {
+    return {count: 0}
+}
