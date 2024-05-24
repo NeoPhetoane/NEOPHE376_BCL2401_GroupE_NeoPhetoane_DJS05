ef4468b2a1661ff67cbd3a0c5b9c60c6094e753a
NeoPhetoane <phetoaneneo4@gmail.com>
2024-05-24 16:08:42 +0200

    createStore which is a state management function has benn created

--- index.js
+++ index.js
@@ -1,30 +1,41 @@
-const initialState = { count: 0 };  //Initial state of the app declared
-
-//Counting functions
-
-// function reducer(state = initialState, actions) {
-//     switch (actions.type) {
-//       case action.INCREMENT:
-//         return { count: state.count + 2 };
-//       case action.DECREMENT:
-//         return { count: state.count - 1 };
-//       case action.RESET:
-//         return { count: 0 };
-//       default:
-//         return state;
-//     }
-//   }
-
+const initialState = { count: 0 }; //Initial state of the app declared
 
 //Action functions created
-  function increment(state) {
-    return { count: state.count + 1 }
+function increment(state) {
+  return { count: state.count + 1 };
 }
-  
+
 function decrement(state) {
-    return { count: state.count - 1 }
+  return { count: state.count - 1 };
 }
 
 function reset() {
-    return {count: 0}
+  return { count: 0 };
+}
+//Store function
+function createStore(initialState) {
+  // state variable is created to hold the current state of the store
+  let state = initialState;
+  //listeners array holds callbacks for when the store changes
+  let listeners = [];
+
+  return {
+    // This method returns the current state of the store
+    getState: () => state,
+    // dispatch processes actions and updates the state
+    dispatch: (action) => {
+      switch (action.type) {
+        //calls the increment function
+        case "INCREMENT":
+          state = increment(state);
+          break;
+          //calls the decrement function
+        case "DECREMENT":
+          state = decrement(state);
+          break;
+        default:
+          break;
+      }
+    },
+  };
 }
